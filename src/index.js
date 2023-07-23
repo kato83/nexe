0c9279c5236a28bc68535730c1b5ca798d56e711
kato.yuki <kato.yuki@securesky-tech.com>
2023-07-24 00:59:25 +0900

    Event 登録時に必要な challenge の部分の実装

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-exports.handler = async function (event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
-  return context.logStreamName;
+exports.handler = async (event) => {
+
+  // handle challenge
+  const challenge = JSON.parse(event?.body || '{}').challenge;
+  if (challenge) {
+    const body = {
+      challenge: challenge
+    };
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(body)
+    };
+    return response;
+  }
+
+  // ログに書き込む
+  console.log(JSON.stringify(event));
+
+  // 200を返す。
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+
 };
