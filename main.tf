b978d6c77da713e4689b6a0802d0cf2c888e628a
kato.yuki <kato.yuki@securesky-tech.com>
2023-07-27 10:48:29 +0900

    main.tf 調整

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  # デプロイしたいリージョンに適宜変更してください
   region = "ap-northeast-1"
 }
 
@@ -13,7 +12,7 @@ variable "SLACK_BOT_TOKEN" {
   default = "nothing"
 }
 
-data "archive_file" "sample_function" {
+data "archive_file" "zip" {
   type        = "zip"
   source_dir  = "dist/js"
   output_path = "dist/index.js.zip"
@@ -21,22 +20,16 @@ data "archive_file" "sample_function" {
 
 # Lambda関数を作成
 resource "aws_lambda_function" "lambda" {
-  # ユニークな関数名を指定してください
   function_name = "nexe"
   role          = aws_iam_role.lambda_exec_role_attach_policy.arn
-  # Node.jsランタイムを指定します
   runtime       = "nodejs18.x"
-  # ハンドラは「ファイル名.エクスポート関数名」の形式で指定します
   handler       = "index.honi"
-  # ZIPファイルのSHA256ハッシュ
-  source_code_hash = "${data.archive_file.sample_function.output_base64sha256}"
-  # アップロードする zip ファイルパス
-  filename  = "${data.archive_file.sample_function.output_path}"
-
+  source_code_hash = data.archive_file.zip.output_base64sha256
+  filename  = data.archive_file.zip.output_path
   environment {
     variables = {
-      SLACK_BOT_TOKEN = "${var.SLACK_BOT_TOKEN}"
-      SLACK_SIGNING_SECRET = "${var.SLACK_SIGNING_SECRET}"
+      SLACK_BOT_TOKEN = var.SLACK_BOT_TOKEN
+      SLACK_SIGNING_SECRET = var.SLACK_SIGNING_SECRET
     }
   }
 }
@@ -59,13 +52,18 @@ resource "aws_iam_role" "lambda_exec_role_attach_policy" {
   })
 }
 
-# Lambda関数にS3バケットへのアクセスを許可するIAMポリシーをアタッチ
-resource "aws_iam_role_policy_attachment" "lambda_s3_access" {
-  # 適切なポリシーARNを指定してください
+# Lambda関数にアクセスを許可するIAMポリシーをアタッチ
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution_access" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.lambda_exec_role_attach_policy.name
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_translate_access" {
+  policy_arn = "arn:aws:iam::aws:policy/TranslateReadOnly"
+  role       = aws_iam_role.lambda_exec_role_attach_policy.name
+}
+
+# function url 作成
 resource "aws_lambda_function_url" "aws_lambda_function_url" {
   function_name      = aws_lambda_function.lambda.function_name
   authorization_type = "NONE"
